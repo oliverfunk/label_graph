ec9cd1c6d8eb237e3ca9c33ebca0fb745955848f
Oliver Funk <oliver.funk@thoughtexpress.com>
2018-10-25 17:58:15 +0200

    First additions

---
+++ examples/make_graph.rs
@@ -0,0 +1,6 @@
+extern crate label_graph;
+
+fn main() {
+    let a = label_graph::add(2,3);
+    println!("{:?}", a);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,31 @@
+/// Directed graph store
+pub struct DirectedGraph<L, D>
+where L: Ord
+{
+    store: Vec<DirectedGraphNode<L, D>>,
+    stored: bool,
+}
+
+/// Node to be used in a directed graph
+pub struct DirectedGraphNode<L, D>
+where L: Ord
+{
+    label: L,
+    connections: Vec<GraphEdge>,
+    data: D,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct NodeIndex {
+    index: usize,
+}
+
+pub struct GraphEdge {
+    from: NodeIndex,
+    to: NodeIndex,
+    weight: i32,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
