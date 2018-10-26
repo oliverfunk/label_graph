b17cc0c4bf4ab4c5a21530eddffa5310a45ca2b8
Oliver Funk <oliver.funk@thoughtexpress.com>
2018-10-26 14:26:41 +0200

    got to a point where I need to access the node label type in the graph implementation, but can't. Going to try a new approach.

--- examples/make_graph.rs
+++ examples/make_graph.rs
@@ -1,6 +1,3 @@
 extern crate label_graph;
 
-fn main() {
-    let a = label_graph::add(2,3);
-    println!("{:?}", a);
-}
+fn main() {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,117 @@
-/// Directed graph store
-pub struct DirectedGraph<L, D>
-where L: Ord
-{
-    store: Vec<DirectedGraphNode<L, D>>,
-    stored: bool,
+pub trait LabelNode {
+    type Label;
+    type Data;
+
+    fn new_node(label: Self::Label, data: Self::Data) -> Self;
+    fn get_label(&self) -> Self::Label;
+    fn attach_connection(&mut self, edge: GraphEdge);
 }
+
+pub trait DirectedNode {
+    /// All nodes that are input into this node
+    fn input_nodes(&self, this_index: NodeIndex) -> Vec<GraphEdge>;
+
+    /// All nodes that this node outputs to
+    fn output_nodes(&self, this_index: NodeIndex) -> Vec<GraphEdge>;
+}
+
+pub trait UndirectedNode {
+    fn get_connected_nodes(&self, this_index: NodeIndex) -> Vec<GraphEdge>;
+}
+
+/// Holds the nodes of the graph
+pub struct LabelGraph<N>
+where
+    N: LabelNode,
+{
+    nodes: Vec<N>,
+    sorted: bool,
+}
+
+impl<N> LabelGraph<N> {
+    pub fn new() -> LabelGraph<N> {
+        LabelGraph {
+            nodes: Vec::new(),
+            sorted: false,
+        }
+    }
+
+    pub fn with_capacity(capacity: usize) -> LabelGraph<N> {
+        LabelGraph {
+            nodes: Vec::with_capacity(capacity),
+            sorted: false,
+        }
+    }
+
+    pub fn add_node(&mut self, node: N) {
+
+    }
+
+    pub fn get_node(&mut self, )
+}
+
 
 /// Node to be used in a directed graph
 pub struct DirectedGraphNode<L, D>
-where L: Ord
+where
+    L: Ord + Clone,
 {
     label: L,
     connections: Vec<GraphEdge>,
     data: D,
 }
 
-#[derive(Debug, Copy, Clone)]
+impl<L, D> DirectedNode for DirectedGraphNode<L, D>
+where
+    L: Ord + Clone,
+{
+    fn input_nodes(&self, this_index: NodeIndex) -> Vec<GraphEdge> {
+        self.connections
+            .iter()
+            .map(|edge| edge.clone())
+            .filter(|edge| edge.to == this_index)
+            .collect()
+    }
+
+    fn output_nodes(&self, this_index: NodeIndex) -> Vec<GraphEdge> {
+        self.connections
+            .iter()
+            .map(|edge| edge.clone())
+            .filter(|c| c.from == this_index)
+            .collect()
+    }
+}
+
+impl<L, D> LabelNode for DirectedGraphNode<L, D>
+where
+    L: Ord + Clone,
+{
+    type Label = L;
+    type Data = D;
+
+    fn new_node(label: Self::Label, data: Self::Data) -> Self {
+        Self {
+            label,
+            connections: Vec::new(),
+            data,
+        }
+    }
+
+    fn get_label(&self) -> L {
+        self.label.clone()
+    }
+
+    fn attach_connection(&mut self, edge: GraphEdge) {
+        self.connections.push(edge);
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct NodeIndex {
     index: usize,
 }
 
+#[derive(Clone)]
 pub struct GraphEdge {
     from: NodeIndex,
     to: NodeIndex,
